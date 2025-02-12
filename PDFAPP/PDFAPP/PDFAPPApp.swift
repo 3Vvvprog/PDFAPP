8b59158e9255f2e0565a2377bfe9a0edb7f9c637
3Vvv <333vovk@mail.ru>
2025-02-12 15:38:24 +0300

    init app

---
+++ PDFAPP/PDFAPP/PDFAPPApp.swift
@@ -0,0 +1,20 @@
+//
+//  PDFAPPApp.swift
+//  PDFAPP
+//
+//  Created by Вячеслав Вовк on 12.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct PDFAPPApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
